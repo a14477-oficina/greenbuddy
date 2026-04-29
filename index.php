620c89adbfa14b6362f783cc8ece8d8417b35dca
a14477-oficina <a14477@oficina.pt>
2026-04-29 07:31:56 +0000

    second commit

---
+++ index.php
@@ -0,0 +1,2 @@
+Estou a ler php ? 
+<?php echo date('Y-m-d H:i:s'); ?>
---
+++ recebe.php
@@ -0,0 +1,37 @@
+<?php
+
+require "db.php"; // ligação à base de dados
+
+// dados vindos do Arduino
+$humidade = $_GET['humidade'] ?? null;
+
+// validação
+if ($humidade === null) {
+    echo "Nenhum dado recebido";
+    exit;
+}
+
+// data e hora separadas
+$data = date("Y-m-d");
+$hora = date("H:i:s");
+
+// INSERT na base de dados
+
+$stmt = $conn->prepare("
+    INSERT INTO vaso_humidade (data, hora, percentagem)
+    VALUES (?, ?, ?)
+");
+
+$stmt->bind_param("ssd", $data, $hora, $humidade);
+
+if ($stmt->execute()) {
+    echo "OK - Guardado na base de dados";
+} else {
+    echo "Erro ao inserir: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+
+
+?>
